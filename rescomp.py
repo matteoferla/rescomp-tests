d00c8f12e775d8dd5fe2e43d5627c37e55008b4c
matteoferla <matteo.ferla@gmail.com>
2022-08-09 17:58:17 +0100

    :truck: add a few funs

--- rescomp.py
+++ rescomp.py
@@ -1,14 +1,27 @@
-from IPython.core.magic import (register_cell_magic)
-import re
 import uuid
-from mako.template import Template
 from typing import Optional
 
-with open('sge.mako', 'r') as fh:
-    template = fh.read()
+from IPython.core.magic import (register_cell_magic)
+from mako.template import Template
 
+import re, os
+from typing import Optional
+from collections import namedtuple
+import warnings
+
+if os.path.exists('sge.mako'):
+    with open('sge.mako', 'r') as fh:
+        template = fh.read()
+else:
+    warnings.warn('No sge.mako found, please fill rescomp.template')
+    template = ''
 
 def uuid_jobname(jobname: Optional[str] = None) -> str:
+    """
+    Generate a jobname if none is provided. with a uuid (utteroverkill)
+    :param jobname:
+    :return:
+    """
     if jobname is None:
         return str(uuid.uuid4().fields[0])
     jobname = str(jobname).strip()
@@ -42,7 +55,7 @@ def rescomp_submit(py_filename: str, py_args: str = '',
 
 
 @register_cell_magic
-def rescomp(line: str, cell: str) -> int:
+def rescomp(line: str, cell: str) -> str:
     """
     Returns job id
     https://www.medsci.ox.ac.uk/divisional-services/support-services-1/bmrc/cluster-usage
@@ -71,6 +84,76 @@ def rescomp(line: str, cell: str) -> int:
     # print(', '.join(re.findall(r'\$\{(.*?)}', template)))
 
 del rescomp
+
+# -----------------------------------------------------------------------------
+
+Job = namedtuple('Job', ['id', 'name'])
+
+
+def get_last_job_id() -> Job:
+    """Jupyter only... The global ``_oh`` won't exist otherwise"""
+    for o in reversed(_oh.values()):
+        if not isinstance(o, str):
+            continue
+        rex = re.match('Your job (\d+) \("(.*)"\) has been submitted', o)
+        if rex:
+            # job_name
+            return Job(id=int(rex.group(1)), name=rex.group(2))
+
+    else:
+        raise ValueError('No jobs found in cell outputs... did the kernel runtime reset?')
+
+
+def get_job_log(job: Optional[Job] = None) -> str:
+    """Given a job namedtuple (or guess it), return the job log"""
+    if job is None:
+        job = get_last_job_id()
+    log_filename = f'{job.name}.o{job.id}'
+    if os.path.exists(log_filename):
+        with open(log_filename) as fh:
+            return fh.read()
+
+#cat /gpfs0/mgmt/modules/Modules/default/init/python.py
+import os, re, subprocess
+
+if os.environ.get('MODULE_VERSION', None) is None:
+    os.environ['MODULE_VERSION_STACK'] = '3.2.10'
+    os.environ['MODULE_VERSION'] = '3.2.10'
+else:
+    os.environ['MODULE_VERSION_STACK'] = os.environ['MODULE_VERSION']
+os.environ['MODULESHOME'] = '/gpfs0/mgmt/modules/Modules/3.2.10'
+
+if os.environ.get('MODULEPATH', None) is None:
+    f = open(os.environ['MODULESHOME'] + "/init/.modulespath", "r")
+    path = []
+    for line in f.readlines():
+        line = re.sub("#.*$", '', line)
+        if line != '':
+            path.append(line)
+    os.environ['MODULEPATH'] = ':'.join(path)
+
+if os.environ.get('LOADEDMODULES', None) is None:
+    os.environ['LOADEDMODULES'] = ''
+
+def module(*args):
+    """
+    Load a module, like ``module('load', 'CUDA')``.
+    This is because doing ``os.system('module load CUDA')`` does not work.
+    From /gpfs0/mgmt/modules/Modules/default/init/python.py
+    made Py 3.6 compatible.
+
+
+    :param args: 'load', 'modulename' generally!
+    :return:
+    """
+    if isinstance(args[0], list):
+        args = args[0]
+    else:
+        args = list(args)
+    (output, error) = subprocess.Popen(['/gpfs0/mgmt/modules/Modules/%s/bin/modulecmd'
+                                        % os.environ['MODULE_VERSION'], 'python'] +
+        args, stdout=subprocess.PIPE).communicate()
+    exec(output)
 
 
 
